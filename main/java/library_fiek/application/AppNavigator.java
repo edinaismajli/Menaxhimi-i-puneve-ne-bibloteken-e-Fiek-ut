4e52b08c5ff2d1ac6d65a42478f189090d8280ca
edinaismajli <edina.ismajli@student.uni-pr.edu>
2026-05-23 16:01:27 -0100

    Add application view routes

---
+++ main/java/library_fiek/application/AppNavigator.java
@@ -0,0 +1,14 @@
+package library_fiek.application;
+
+public final class AppNavigator {
+    public static final String DASHBOARD = "/library_fiek/views/dashboard-view.fxml";
+    public static final String BOOKS = "/library_fiek/views/books-view.fxml";
+    public static final String MEMBERS = "/library_fiek/views/members-view.fxml";
+    public static final String LOANS = "/library_fiek/views/loans-view.fxml";
+    public static final String DATABASE = "/library_fiek/views/database-view.fxml";
+    public static final String HELP = "/library_fiek/views/help-view.fxml";
+    public static final String SETTINGS = "/library_fiek/views/settings-view.fxml";
+
+    private AppNavigator() {
+    }
+}
