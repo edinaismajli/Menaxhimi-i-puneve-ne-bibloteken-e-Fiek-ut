76b47d06a78b464998966db76337314391097c0f
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-29 13:49:18 +0200

    Add dashboard statistics data classes

---
+++ main/java/library_fiek/models/DashboardStats.java
@@ -0,0 +1,31 @@
+﻿package library_fiek.models;
+
+public class DashboardStats {
+    private int totalBooks;
+    private int totalMembers;
+    private int activeLoans;
+    private int overdueLoans;
+
+    public DashboardStats(int totalBooks, int totalMembers, int activeLoans, int overdueLoans) {
+        this.totalBooks = totalBooks;
+        this.totalMembers = totalMembers;
+        this.activeLoans = activeLoans;
+        this.overdueLoans = overdueLoans;
+    }
+
+    public int getTotalBooks() {
+        return totalBooks;
+    }
+
+    public int getTotalMembers() {
+        return totalMembers;
+    }
+
+    public int getActiveLoans() {
+        return activeLoans;
+    }
+
+    public int getOverdueLoans() {
+        return overdueLoans;
+    }
+}
