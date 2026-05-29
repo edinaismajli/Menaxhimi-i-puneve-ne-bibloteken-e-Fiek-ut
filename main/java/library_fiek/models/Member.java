4f26fbb97240afaac8e572a2016a33033ba0fbcf
blertalatifi <blerta.latifi4@student.uni-pr.edu>
2026-05-29 13:50:51 +0200

    add member model

--- main/java/library_fiek/models/Member.java
+++ main/java/library_fiek/models/Member.java
@@ -79,6 +79,30 @@ package library_fiek.mappers;
 import library_fiek.dto.MemberDto;
 import library_fiek.models.Member;
 
+public class MemberMapper {
+    public Member toModel(MemberDto dto) {
+        return new Member(
+                0,
+                dto.getFullName(),
+                dto.getEmail(),
+                dto.getPhone(),
+                dto.getMemberType()
+        );
+    }
+
+    public Member toModel(int id, MemberDto dto) {
+        return new Member(
+                id,
+                dto.getFullName(),
+                dto.getEmail(),
+                dto.getPhone(),
+                dto.getMemberType()
+        );
+    }
+}
+
+
+
 
 
 
