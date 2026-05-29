ca6db4c9c2e38e97d2b9d97e5a91a35192264104
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-29 13:49:19 +0200

    Add shared database connection service

---
+++ main/java/library_fiek/services/DatabaseService.java
@@ -0,0 +1,13 @@
+﻿package library_fiek.services;
+
+import library_fiek.database.DatabaseConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DatabaseConfig.DATABASE_URL);
+    }
+}
