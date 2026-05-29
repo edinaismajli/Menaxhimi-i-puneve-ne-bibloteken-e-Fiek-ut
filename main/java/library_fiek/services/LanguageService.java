dd7d0e0117d210ea346100ffaf1e9be681af2cad
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-29 13:49:19 +0200

    Add language and accessibility services

---
+++ main/java/library_fiek/services/LanguageService.java
@@ -0,0 +1,31 @@
+﻿package library_fiek.services;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LanguageService {
+    private static String currentLanguage = "sq";
+    private static ResourceBundle bundle = loadBundle(currentLanguage);
+
+    public static void setLanguage(String languageCode) {
+        currentLanguage = languageCode;
+        bundle = loadBundle(languageCode);
+    }
+
+    public static String getCurrentLanguage() {
+        return currentLanguage;
+    }
+
+    public static String get(String key) {
+        try {
+            return bundle.getString(key);
+        } catch (Exception e) {
+            return key;
+        }
+    }
+
+    private static ResourceBundle loadBundle(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        return ResourceBundle.getBundle("library_fiek.lang.messages", locale);
+    }
+}
