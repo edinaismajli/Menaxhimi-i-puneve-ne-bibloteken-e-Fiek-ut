3236a29fb9d30291556d4cc41888c45dc537a93f
dielsamaloku <dielsa.maloku@student.uni-pr.edu>
2026-05-29 13:49:19 +0200

    Add help and settings controllers

---
+++ main/java/library_fiek/controllers/HelpController.java
@@ -0,0 +1,15 @@
+﻿package library_fiek.controllers;
+
+import library_fiek.services.LanguageService;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class HelpController {
+    @FXML
+    private TextArea helpTextArea;
+
+    @FXML
+    public void initialize() {
+        helpTextArea.setText(LanguageService.get("help.text"));
+    }
+}
